b7d09d69f4a2752c4e04757c7e99cd11248b8cbf
Mimie <berylsamba9@gmail.com>
2024-11-21 20:20:02 +0100

    added public/index.html folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+   return (
+    <>
+     <Router>
+     <div className='nav'>
+      <div className='logo'>Berylsamba.</div>
+          <ul className="menu">
+            <li><Link to="/">Home</Link></li> 
+            <li><Link to="/about">About Me</Link></li>
+            <li><Link to="/contact">Contact</Link></li> 
+          </ul>
+     </div>
+     <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+     </Router>
+
+    </>
+  )
 }
-
-export default App;
